5a33a0b8015c11398d45899044db77bdb1631c35
Amelie <amelietdavies@gmail.com>
2026-03-24 14:07:24 +0000

    Improved documentation and stripped out unneeded data

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # causal-cut-tool
-Implementation of Causal Cut library as a useable tool
+Implementation of the Causal Cut library as a usable tool. 
+Run with `./causal_cut.sh datafile.pqt`.
+## Environment setup
+This project uses uv to manage the python environment. Setup the
+environment by installing uv and then (in the causal_cut_tool directory) 
+running:
+
+`uv pip install -e .`
+
+`uv run bash ./causal_cut.sh [DATAFILE TO TEST].pqt`
+
+## Adapting the tool
+In order to run your system using the tool, you will need to allow the tool to call a method that can run your system 
+(or model of your system). This can be done in the causal_cut_plugin directory. Details about the implementation needed
+can be found in the tutorial.ipynb file along with information about the data that needs to be provided.
+
+TODO:
+- Descriptions of datafiles
+- Strip down datafiles
--- causal_cut_tool/causal_cut_plugin/__init__.py
+++ causal_cut_tool/causal_cut_plugin/__init__.py
@@ -1,51 +1,18 @@
-from typing import Any
-
 import matplotlib.pyplot as plt
 import pandas as pd
-from aps_digitaltwin.model import Model
-from aps_digitaltwin.openaps import OpenAPS
-from aps_digitaltwin.util import s_label, j_label, l_label, g_label, i_label, BOLUS, BASAL_BOLUS_THRESHOLD
-from aps_digitaltwin.util import LOW, HIGH, BASAL_BOLUS_THRESHOLD, BOLUS, SNACK, LIGHT_MEAL, HEAVY_MEAL, constant_names
+
+HIGH = 1
+LOW = 0
 
 def _plot(df, timesteps):
     fig, (ax1, ax2) = plt.subplots(1, 2)
     ax1.set_title("No Intervention")
     ax1.hlines(y=[HIGH, LOW], xmin=0, xmax=timesteps, colors="r", linestyles="--", lw=1)
-    ax2.set_title("OpenAPS Intervention")
+    ax2.set_title("With Intervention")
     ax2.hlines(y=[HIGH, LOW], xmin=0, xmax=timesteps, colors="r", linestyles="--", lw=1)
 
-    df.plot("step", [s_label, j_label, l_label, g_label, i_label], ax=ax2)
+    df.plot("step", ax=ax2)
     plt.show()
-
-def run_control(
-    interventions, list,
-    constants: list,
-    timesteps: int,
-    timesteps_per_intervention: int,
-    plot=False,
-    kill_at_fault=False,
-    initial_values=None,
-):
-    model_control = Model(initial_values, constants)
-
-    for intervention in interventions:
-        model_control.add_intervention(intervention[0], intervention[1], intervention[2])
-
-    control_violations = []
-    fault = False
-    for t in range(1, timesteps + 1):
-        timestep = model_control.update(t)
-        if not (LOW < timestep[g_label] < HIGH):
-            fault = True
-            control_violations.append(timestep["step"])
-            if kill_at_fault and fault and t % timesteps_per_intervention == 0:
-                break
-
-    control_df = pd.DataFrame(model_control.history)
-
-    if plot:
-        _plot(control_df, timesteps)
-    return control_df
 
 def run(
     attack: dict,
@@ -54,59 +21,44 @@ def run(
     constants: list,
     plot=False,
     model_control=False,
-    tempdir="openaps_temp",
+    tempdir="temp",
     kill_at_fault=False,
     timesteps_per_intervention=5,
 ):
+    """
 
-    initial_values: list[float] = [attack["initial_carbs"], 0, 0, attack["initial_bg"], attack["initial_iob"]]
-
-    if model_control:
-        control_df = run_control(constants,
-                                 plot=plot,
-                                 kill_at_fault=kill_at_fault,
-                                 timesteps=timesteps,
-                                 timesteps_per_intervention=timesteps_per_intervention,
-                                 initial_values=initial_values)
-
-    open_aps = OpenAPS(None, profile_path=None, basal_profile_path=None)
-    model_openaps = Model(initial_values, constants, interventions=interventions)
-
+    :param: attack: Attack dictionary containing all data calculated during g-estimation and during data generation
+    :param: timesteps: Number of timesteps to simulate
+    :param: interventions: List containing all possible interventions
+    :param: constants: List of all simulation constants
+    :param: plot: Whether to plot the data
+    :param: model_control: Whether to generate a control simulation
+    :param: tempdir: Path to temporary directory
+    :param: kill_at_fault: Should system terminate if a fault is caused
+    :param: timesteps_per_intervention: How many timesteps to simulate between interventions
+    :return: Dataframe containing simulation history
+    """
+    # Setup systems under test
     fault = False
     for t in range(1, timesteps + 1):
         if t % timesteps_per_intervention == 0:
-            rate = open_aps.run(model_openaps.history, tempdir)
-            if rate > BASAL_BOLUS_THRESHOLD:
-                model_openaps.add_intervention(t, "bolus", BOLUS)
-            else:
-                for j in range(5):
-                    model_openaps.add_intervention(t + j, i_label, rate / 5.0)
-        timestep = model_openaps.update(t)
-        fault = fault or (not (LOW < timestep[g_label] < HIGH))
+            break
+            # Add intervention(s)
+
+        # Update modelled system
+        fault = fault or True # See if fault has developed
+
+        # Break at fault if needed
         if kill_at_fault and fault and t % timesteps_per_intervention == 0:
             break
-    openaps_df = pd.DataFrame(model_openaps.history)
-    openaps_df["Safe"] = openaps_df["Blood Glucose"].between(LOW, HIGH)
 
-    hyper_violations = []
-    hypo_violations = []
-    bg_change = 0
-    for idx, timestep in enumerate(model_openaps.history):
-        if not idx == 0:
-            bg_change += abs(timestep[g_label] - model_openaps.history[idx - 1][g_label])
-        if timestep[g_label] > HIGH:
-            hyper_violations.append(timestep["step"])
-
-        if timestep[g_label] < LOW:
-            hypo_violations.append(timestep["step"])
-
+    # Export model history from system
+    df = pd.DataFrame()
+    df["Safe"] = []
     if plot:
-        _plot(openaps_df, timesteps)
+        _plot(df, timesteps)
 
-    for k, v in zip(constant_names, constants):
-        openaps_df[k] = v
-
-    return openaps_df
+    return df
 
 def find_faults(sim_df):
     """
@@ -114,9 +66,13 @@ def find_faults(sim_df):
     :param sim_df:
     :return: fault, fault_time
     """
-    unsafe = sim_df.loc[~sim_df["Safe"], "step"]
+    # Filter failing runs
+    unsafe = sim_df.loc[sim_df, "step"]
     fault_time = int(unsafe.min())
-    glucose_value = sim_df.loc[fault_time, "Blood Glucose"]
-    fault = "Low" if glucose_value < LOW else "High" if glucose_value > HIGH else None
-    assert fault is not None
-    return fault, fault_time
+
+    # Find cause of fault, e.g.
+    # val = sim_df[fault_time].val
+    # fault = "Low" if val < LOW else "High" if val > HIGH else None
+    #assert fault is not None
+
+    return "FAULT", fault_time
